74dde4ad65730c2fc7af49935ab23054d985f90a
skyairwater <skyairwater@gmail.com>
2026-02-03 18:46:40 -0500

    Added HttpClient, Created StockSymbol controller

---
+++ Backend/Finance.Api/Finance.Api/Controllers/StockSymbols.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StockSymbols : ControllerBase
+{
+    public IEnumerable<(string tradingSymbol, string registrantName)>? Get()
+    {
+        return null;
+    }
+}
