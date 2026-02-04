4ad5739ed34760cd73d37c575c272375a627d582
skyairwater <skyairwater@gmail.com>
2026-02-03 21:26:19 -0500

    Implemented different endpoints to get stock symbols

--- Backend/Finance.Api/Finance.Api/Controllers/StockSymbolController.cs
+++ Backend/Finance.Api/Finance.Api/Controllers/StockSymbolController.cs
@@ -7,12 +7,53 @@ namespace Finance.Api.Controllers;
 [ApiController]
 public class StockSymbolController(IHttpClientFactory factory, IConfiguration configuration) : ControllerBase
 {
-    public async Task<IEnumerable<StockDetail>?> Get()
+    [HttpGet("GetAll")]
+    public async Task<IEnumerable<StockDetail>?> GetAll()
     {
         var client = factory.CreateClient("get all stock symbols");
 
         var requestUriWithApiKey = $"https://financialdata.net/api/v1/stock-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
 
         return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);         
+    }
+
+    [HttpGet("GetInternationalSymbols")]
+    public async Task<IEnumerable<StockDetail>?> GetInternationalSymbols()
+    {
+        var client = factory.CreateClient("get international symbols");
+
+        var requestUriWithApiKey = $"https://financialdata.net/api/v1/international-stock-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
+
+        return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);
+    }
+
+    [HttpGet("GetEtfSymbols")]
+    public async Task<IEnumerable<StockDetail>?> GetEtfSymbols()
+    {
+        var client = factory.CreateClient("get Etf symbols");
+
+        var requestUriWithApiKey = $"https://financialdata.net/api/v1/etf-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
+
+        return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);
+    }
+
+    [HttpGet("GetCommoditySymbols")]
+    public async Task<IEnumerable<StockDetail>?> GetCommoditySymbols()
+    {
+        var client = factory.CreateClient("get commodity symbols");
+
+        var requestUriWithApiKey = $"https://financialdata.net/api/v1/commodity-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
+
+        return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);
+    }
+
+    [HttpGet("GetOverTheCounterSymbols")]
+    public async Task<IEnumerable<StockDetail>?> GetOverTheCounterSymbols()
+    {
+        var client = factory.CreateClient("get OTC symbols");
+
+        var requestUriWithApiKey = $"https://financialdata.net/api/v1/commodity-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
+
+        return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);
     }
 }
--- Backend/Finance.Api/Finance.Api/Models/StockDetail.cs
+++ Backend/Finance.Api/Finance.Api/Models/StockDetail.cs
@@ -9,4 +9,10 @@ public class StockDetail
 
     [JsonPropertyName("registrant_name")]
     public string RegistrantName { get; set; } = string.Empty;
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; } = string.Empty;
+
+    [JsonPropertyName("title_of_security")]
+    public string SecurityTitle { get; set; } = string.Empty;
 }
