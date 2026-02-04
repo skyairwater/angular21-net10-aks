2d0eab47f8b14daddbf68d889046d92b5d372d4e
skyairwater <skyairwater@gmail.com>
2026-02-03 21:02:27 -0500

    implemented /api/StockSymbol/Get

---
+++ Backend/Finance.Api/Finance.Api/Controllers/StockSymbolController.cs
@@ -0,0 +1,18 @@
+﻿using Finance.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StockSymbolController(IHttpClientFactory factory, IConfiguration configuration) : ControllerBase
+{
+    public async Task<IEnumerable<StockDetail>?> Get()
+    {
+        var client = factory.CreateClient("get all stock symbols");
+
+        var requestUriWithApiKey = $"https://financialdata.net/api/v1/stock-symbols?key={configuration.GetValue<string>("FinancialDataApiKey")}";
+
+        return await client.GetFromJsonAsync<IEnumerable<StockDetail>>(requestUri: requestUriWithApiKey);         
+    }
+}
---
+++ Backend/Finance.Api/Finance.Api/Models/StockDetail.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Finance.Api.Models;
+
+public class StockDetail
+{
+    [JsonPropertyName("trading_symbol")]
+    public string TradingSymbol { get; set; } = string.Empty;
+
+    [JsonPropertyName("registrant_name")]
+    public string RegistrantName { get; set; } = string.Empty;
+}
